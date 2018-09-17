5353cb726f650ff08ba892c5174cfd13c0987d3e
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2018-09-16 21:00:02 -0400

    Added more queries

--- MySQL/queries.sql
+++ MySQL/queries.sql
@@ -9,7 +9,7 @@ SELECT * FROM City
 WHERE Countrycode='USA' and Population>100000;
 
 -- Query all columns (attributes) for every row in the CITY table --
-SELECT * FROM CITY
+SELECT * FROM CITY;
 
 -- Query all columns for a city in CITY with the ID 1661 --
 SELECT * FROM CITY
@@ -18,3 +18,7 @@ WHERE ID = 1661;
 -- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN --
 SELECT NAME FROM CITY
 WHERE COUNTRYCODE = 'JPN'; 
+
+-- Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA --
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
