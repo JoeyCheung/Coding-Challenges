940976ce1740b83cba1380ffdc6e4531b8d7318f
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2018-09-17 14:38:30 -0400

    Edited previous commit

--- MySQL/Hackerrank.sql
+++ MySQL/Hackerrank.sql
@@ -41,13 +41,13 @@ SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
 FROM STATION;
 
 -- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically --
-SELECT City, LENGTH(City)
-FROM (SELECT City
-      FROM Station
-     ORDER BY LENGTH(City), City)
+SELECT CITY, LENGTH(CITY)
+FROM (SELECT CITY
+      FROM STATION
+     ORDER BY LENGTH(CITY), CITY)
 WHERE ROWNUM = 1;
-SELECT City, LENGTH(City)
-FROM (SELECT City
-      FROM Station
-     ORDER BY LENGTH(City) DESC, City)
+SELECT CITY, LENGTH(CITY)
+FROM (SELECT CITY
+      FROM STATION
+     ORDER BY LENGTH(CITY) DESC, CITY)
 WHERE ROWNUM = 1;
