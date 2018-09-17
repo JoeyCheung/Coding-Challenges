8a55c55e72133c67362e86e211bbca57384a3b6b
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2018-09-17 14:27:54 -0400

    More

--- MySQL/Hackerrank.sql
+++ MySQL/Hackerrank.sql
@@ -35,3 +35,7 @@ FROM STATION
 SELECT DISTINCT CITY 
 FROM STATION 
 WHERE MOD(STATION.ID,2)=0;
+
+-- Let be the number of CITY entries in STATION, and let be the number of distinct CITY names in STATION; query the value of from STATION. In other words, find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table --
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
