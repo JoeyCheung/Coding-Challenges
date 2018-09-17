1da35fe1d57fd285f22b49a266ff5704f4db0979
Joey Cheung <joeycheung0@gmail.com>
2018-09-16 20:57:12 -0400

    Added SQL folder

---
+++ MySQL/queries.sql
@@ -0,0 +1,20 @@
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates --
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE substr(CITY,1,1) NOT IN ('A','E','I','O','U');
+
+-- Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA  --
+SELECT * FROM City 
+WHERE Countrycode='USA' and Population>100000;
+
+-- Query all columns (attributes) for every row in the CITY table --
+SELECT * FROM CITY
+
+-- Query all columns for a city in CITY with the ID 1661 --
+SELECT * FROM CITY
+WHERE ID = 1661;
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN --
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'JPN'; 
