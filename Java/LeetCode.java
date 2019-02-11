c242885d1a883ea376d6e1f5866b2628c9bddf7d
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-02-11 00:30:23 -0500

    Update LeetCode.java

--- Java/LeetCode.java
+++ Java/LeetCode.java
@@ -1,16 +1,18 @@
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-public int[] twoSum(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return new int[] [ i, j ];
+    
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
             }
         }
+        throw new IllegalArgumentException("No two sum solution");
     }
-    throw new IllegalArgumentException("No two sum solution");
 }
 
 Given an array nums and a value val, remove all instances of that value in-place and return the new length.
