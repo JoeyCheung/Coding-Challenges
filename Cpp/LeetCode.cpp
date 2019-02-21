6b0fc5779d4a02866e89f83093334382510ff387
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-02-21 16:16:17 -0500

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -13,3 +13,27 @@ public:
         else return false;
     }
 };
+
+Write a function to find the longest common prefix string amongst an array of strings. 
+If there is no common prefix, return an empty string "".
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty() == 1) return "";
+        else {
+            int min = strs[0].length();
+            for (int i = 1; i < strs.size(); i++) {
+                if (min > strs[i].length())
+                    min = strs[i].length();
+                for (int j = 0; j < min; j++) {
+                    if (strs[i][j] != strs[i - 1][j]) {
+                        min = j;
+                        break;
+                    }
+                }
+            }
+            return strs[0].substr(0, min);
+        }
+    }
+};
