dc4b2e936f8d42441b8b22851ae5c9d002483c15
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-02-24 12:30:55 -0500

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -64,3 +64,23 @@ public:
         reverse(s.begin(), s.end());
     }
 };
+
+Reverse a singly linked list.
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* p = NULL;
+        ListNode* c = head;
+        ListNode* n;
+        
+        while (c != NULL) {
+            n = c->next;
+            c->next = p;
+            p = c;
+            c = n;
+        }
+        
+        return p;
+    }
+};
