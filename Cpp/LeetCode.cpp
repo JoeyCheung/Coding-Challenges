0661f5eb4e61224ed67a8cffd89ddd4dd0493e56
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-02-23 22:46:37 -0500

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -37,3 +37,19 @@ public:
         }
     }
 };
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+If there are two middle nodes, return the second middle node.
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
