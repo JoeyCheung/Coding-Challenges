1873d2f4f7e15fde745cdaba879819f0bb21f892
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-03-01 23:11:20 -0500

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -94,3 +94,34 @@ public:
         return str;
     }
 };
+
+Given a n-ary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) 
+            return 0; 
+        int maxdepth = 0; 
+        for (vector<Node*>::iterator it = root->children.begin(); it != root->children.end(); it++) {
+            maxdepth = max(maxdepth, maxDepth(*it)); 
+        }
+    return maxdepth + 1 ; 
+    }
+};
