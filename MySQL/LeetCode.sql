461be905dc0f11645a15192effc373c8c8a271c5
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-05-31 18:20:43 -0400

    Update LeetCode.sql

--- MySQL/LeetCode.sql
+++ MySQL/LeetCode.sql
@@ -25,5 +25,5 @@ For example, according to the above table, we should output:
 +--------------+-------------+--------------+
 */
 
-SELECT * FROM World 
+SELECT name, population, area FROM World 
 WHERE area > 3000000 OR population > 25000000;
