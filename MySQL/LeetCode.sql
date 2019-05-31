3118a87ca6a0095605e833f370fa09014bb33bb9
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-05-31 18:19:45 -0400

    Update LeetCode.sql

--- MySQL/LeetCode.sql
+++ MySQL/LeetCode.sql
@@ -1,3 +1,4 @@
+/*
 There is a table World
 
 +-----------------+------------+------------+--------------+---------------+
@@ -22,7 +23,7 @@ For example, according to the above table, we should output:
 | Afghanistan  | 25500100    | 652230       |
 | Algeria      | 37100000    | 2381741      |
 +--------------+-------------+--------------+
-
+*/
 
 SELECT * FROM World 
 WHERE area > 3000000 OR population > 25000000;
