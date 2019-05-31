259d1570bc10c36447d2aab19cff7d82e567ea0d
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-05-31 17:21:52 -0400

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -125,3 +125,27 @@ public:
     return maxdepth + 1 ; 
     }
 };
+
+/*
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+Each character in S is a type of stone you have.  
+You want to know how many of the stones you have are also jewels.
+The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) { 
+        int ans  = 0;
+        for(int i = 0; i < S.length(); i++){
+            for(int j = 0; j < J.length(); j++){
+                if(J[j] == S[i]){
+                    ans ++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
