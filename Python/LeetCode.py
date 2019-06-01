85179afac60b469e2bbd97c9bd5ace990441cf23
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-01 19:19:05 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -18,12 +18,24 @@ class Solution:
         
         if root is None:
             return root
- 
+        
         self.invertTree(root.left)
         self.invertTree(root.right)
- 
+        
         t = root.left
         root.left = root.right
         root.right = t
         
         return root
+
+#Given an array A of distinct integers sorted in ascending order, return the smallest index i that satisfies A[i] == i.  
+#Return -1 if no such i exists.    
+    
+class Solution:
+    def fixedPoint(self, A: List[int]) -> int:
+        length = len(A)
+        for i in range(length): 
+            if A[i] == i: 
+                return i 
+        return -1
+    
