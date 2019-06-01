6403b437fe29aad79cf855e5484057c059526492
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-01 11:26:30 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -3,3 +3,27 @@
 class Solution:
     def toLowerCase(self, str: str) -> str:
         return str.lower()
+
+#Invert Binary Tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        
+        if root is None:
+            return root
+ 
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+ 
+        t = root.left
+        root.left = root.right
+        root.right = t
+ 
+        return root
