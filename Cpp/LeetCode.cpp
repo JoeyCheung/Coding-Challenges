be9674f6634255d3123ba293bc6724d5f51f983c
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-01 11:38:47 -0400

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -226,6 +226,84 @@ public:
     }
 };
 
+/*
+Design your implementation of the linked list. You can choose to use the singly linked list or the doubly linked list. A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node. If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement these functions in your linked list class:
+
+    get(index) : Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+    addAtHead(val) : Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+    addAtTail(val) : Append a node of value val to the last element of the linked list.
+    addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+    deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
+*/
+
+class MyLinkedList {
+private:
+    struct Node {
+        int val;
+        Node *next;
+        Node(int x, Node* n): val(x), next(n) {}
+    };
+    Node *head, *tail;
+    int size;    
+    
+public:
+    /** Initialize your data structure here. */
+    MyLinkedList() {
+        head = NULL;
+        size = 0;
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node *cur = head;
+        for (int i = 0; i < index; ++i) cur = cur->next;
+        return cur->val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    void addAtHead(int val) {
+        Node *t = new Node(val, head);
+        head = t;
+        ++size;
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    void addAtTail(int val) {
+        Node *cur = head;
+        while (cur->next) cur = cur->next;
+        cur->next = new Node(val, NULL);
+        ++size;
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        if (index == 0) {addAtHead(val); return;}
+        Node *cur = head;
+        for (int i = 0; i < index - 1; ++i) cur = cur->next;
+        Node *t = new Node(val, cur->next);
+        cur->next = t;
+        ++size;
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head->next;
+            --size;
+            return;
+        }
+        Node *cur = head;
+        for (int i = 0; i < index - 1; ++i) cur = cur->next;
+        cur->next = cur->next->next;
+        --size;
+    }
+};
+
 // invert a binary tree
 
 /**
