e9c22090a62ecaedaa699a3474b4b21c1d2cb034
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-01 10:47:09 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -1,4 +1,4 @@
-Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+#Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
 
 class Solution:
     def toLowerCase(self, str: str) -> str:
