d8f1c46a907f36a00b531d0042569bd84cd74e48
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-02 00:49:57 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -39,7 +39,7 @@ class Solution:
                 return i 
         return -1
     
-Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+#Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
     
 class Solution:
     def reverseWords(self, s: str) -> str:
