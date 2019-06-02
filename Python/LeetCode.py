0599d56b9e4ff9564d668edfff11ef62b45dcac9
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-02 11:09:46 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -49,3 +49,52 @@ class Solution:
         s_split = s.split(' ')
         s_reverse = [reverseWord(i) for i in s_split]
         return ' '.join(s_reverse)
+
+'''
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its minimum depth = 2.
+
+'''
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        # Corner Case.Should never be hit unless the code is  
+        # called on root = NULL 
+        if root is None: 
+            return 0 
+        
+        # Base Case : Leaf node.This acoounts for height = 1 
+        if root.left is None and root.right is None: 
+            return 1
+        
+        # If left subtree is Null, recur for right subtree 
+        if root.left is None: 
+            return minDepth(root.right)+1
+        
+        # If right subtree is Null , recur for left subtree 
+        if root.right is None: 
+            return minDepth(root.left) +1 
+        
+        return min(minDepth(root.left), minDepth(root.right))+1
