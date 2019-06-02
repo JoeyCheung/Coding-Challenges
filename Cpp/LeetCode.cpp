c94e82f6821b5f897a92fbd9150250bb11ea9e89
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-02 10:40:35 -0400

    Update LeetCode.cpp

--- Cpp/LeetCode.cpp
+++ Cpp/LeetCode.cpp
@@ -334,3 +334,55 @@ public:
         return root;
     }
 };
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its minimum depth = 2.
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        // Corner case. Should never be hit unless the code is  
+        // called on root = NULL 
+        if (root == NULL) 
+            return 0; 
+  
+        // Base case : Leaf Node. This accounts for height = 1. 
+        if (root->left == NULL && root->right == NULL) 
+        return 1; 
+  
+        // If left subtree is NULL, recur for right subtree 
+        if (!root->left) 
+        return minDepth(root->right) + 1; 
+  
+        // If right subtree is NULL, recur for left subtree 
+        if (!root->right) 
+        return minDepth(root->left) + 1; 
+  
+        return min(minDepth(root->left), minDepth(root->right)) + 1;       
+    }
+};
