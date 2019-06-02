3f51c3168aa4d4498480861cd668a33f110c18fa
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-06-02 00:49:46 -0400

    Update LeetCode.py

--- Python/LeetCode.py
+++ Python/LeetCode.py
@@ -39,3 +39,13 @@ class Solution:
                 return i 
         return -1
     
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+    
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        def reverseWord(word):
+            return word[::-1]
+    
+        s_split = s.split(' ')
+        s_reverse = [reverseWord(i) for i in s_split]
+        return ' '.join(s_reverse)
